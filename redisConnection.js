630097bc3b46b16efda2726eff66c54afd0bea73
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-11-21 17:21:59 -0800

    remove connection msg

--- redisConnection.js
+++ redisConnection.js
@@ -4,7 +4,6 @@ export async function connectToRedis() {
   const client = createClient();
   client.on('error', (err) => console.log('Redis Client Error', err));
 
-  console.log('Connecting to Redis...');
   await client.connect();
   return client;
 }
