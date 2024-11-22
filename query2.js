3a1faa73d34202e343e2765e77ccea43eb08af93
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-11-21 17:22:48 -0800

    minimize instruction

--- query2.js
+++ query2.js
@@ -1,8 +1,6 @@
 /**
- * Compute and print the total number of favorites in the dataset. For this
- * apply the same process as before, query all the tweets, start a favoritesSum
- * key (SET), increment it by the number of favorites on each tweet (INCRBY),
- * and then get the value (GET) and print it on the screen.
+ * Compute and print the total number of favorites in the dataset.
+ *
  */
 import { getTweets } from './getTweets.js';
 import { connectToRedis } from './redisConnection.js';
