4e6ada73b08710e0a2455a749c48031617409dfc
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-11-21 19:59:47 -0800

    reduce instruction

--- query4.js
+++ query4.js
@@ -1,6 +1,6 @@
 /**
  * Create a leaderboard with the top 10 users with more tweets.
- * Use a sorted set called leaderboard
+ *
  */
 import { getTweets } from './getTweets.js';
 import { connectToRedis } from './redisConnection.js';
